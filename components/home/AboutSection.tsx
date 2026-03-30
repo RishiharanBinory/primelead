7149d9b8cb715b0b08cc778a1595a2f9de6b46c2
ramanathanrishiharan <ramanathanrishiharan2003@gmail.com>
2026-03-30 09:02:35 +0530

    changed the font size

--- components/home/AboutSection.tsx
+++ components/home/AboutSection.tsx
@@ -28,7 +28,7 @@ export default function AboutSection() {
             </AnimateOnScroll>
 
             <AnimateOnScroll delay="0.35s">
-              <p className="text-sm md:text-base text-[#374151] leading-relaxed text-justify mb-3 md:mb-4">
+              <p className="text-[18px] md:text-base text-[#374151] leading-relaxed text-justify mb-3 md:mb-4">
                 <strong>
                   Prime Leed is a trusted resource for students seeking to apply
                   for higher education.
