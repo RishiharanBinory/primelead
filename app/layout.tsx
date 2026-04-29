d6acff8067f51df1adca18042028f167e0ca8822
ramanathanrishiharan <ramanathanrishiharan2003@gmail.com>
2026-04-29 23:15:42 +0530

    google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { Footer } from "@/components/mainComponents/Footer";
 import FloatingContact from "@/components/mainComponents/Floatingcontact";
 import ZohoSalesIQ from "@/components/mainComponents/ZohoSalesIQ";
 
-
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -24,9 +23,31 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      {/* ✅ ADD THIS */}
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W3GXHDBR');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* ✅ ADD THIS */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3GXHDBR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Navbar />
         <main className="pt-17.5">
           {children}
@@ -34,7 +55,7 @@ export default function RootLayout({
             whatsappNumber="447520604047"
             messengerUsername="primeleed"
           /> */}
-          <ZohoSalesIQ/>
+          <ZohoSalesIQ />
         </main>
         <Footer />
       </body>
